d66c40a1a1faaedd425e5541df2a6484c10e5304
Brandon Birkholz <brandonjbirkholz@gmail.com>
2020-07-31 22:22:33 -0700

    Add basic role management

--- README.md
+++ README.md
@@ -8,6 +8,9 @@ _A discord bot for Northernlion's subscriber server that helps keep his subscrib
 * [ ] YouTube video upload notifications to per-series channels (e.g. NL tries goes to #nl-tries)
 * [ ] Twitch live notifications
 
+#### Role Management
+In the `#roles` channel, the bot has messages posted. On each message, there is a single reaction. Clicking the reaction (i.e., incrementing the reaction) causes the bot to add a role to the user. Clicking the reaction again (to decrement) will remove the role from the user.
+
 ### Commands
 
-
+`!lion roles` posts the roles management message. The bot will delete the command message.
--- bot.py
+++ bot.py
@@ -1,14 +1,47 @@
+import logging
 import os
 
 import discord
+
+logging.basicConfig(level=logging.INFO)
+
+fake_db = {}
 
 class LionBot(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
 
+    def populate_role_id(self, roles):
+        if fake_db.get('role_id') == None:
+            for g_role in roles:
+                if g_role.name == "twitch": # make configurable
+                    role = g_role
+                    fake_db['role_id'] = role.id
+
+    async def on_reaction_add(self, reaction, user):
+        if user == self.user or reaction.message.id != fake_db.get('notif_message_id'):
+            return
+        self.populate_role_id(reaction.message.guild.roles)
+        role = reaction.message.guild.get_role(role_id=fake_db['role_id'])
+        await user.add_roles(role, reason="Reacted to role message.")
+
+    async def on_reaction_remove(self, reaction, user):
+        if reaction.message.id != fake_db.get('notif_message_id'):
+            return
+        self.populate_role_id(reaction.message.guild.roles)
+        role = reaction.message.guild.get_role(role_id=fake_db['role_id'])
+        await user.remove_roles(role, reason="Reacted to role message.")
+
     async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
-        await message.channel.send('Hello')
+        if message.content == '!lion roles':
+            # post message
+            notif_message = await message.channel.send(
+                'React to this message to subscribe to notifications. Remove your reaction to unsubscribe.'
+            )
+            fake_db['notif_message_id'] = notif_message.id
+            # Add reactions
+            await notif_message.add_reaction('👍')
+            await message.delete()
 
 client = LionBot()
 client.run(os.environ.get('DISCORD_TOKEN'))
